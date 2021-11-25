ca693df65ce597199ac5ebd90b085cec7bc87ecd
swanserquack <75944176+swanserquack@users.noreply.github.com>
2021-11-25 16:25:59 +0000

    Added silent updater for the updater file and updated requirements

--- Convertor.py
+++ Convertor.py
@@ -1,8 +1,9 @@
 import requests
-import json
 import colorama
+import ujson
+import filecmp
 import os
-from json import load
+import shutil
 from os import path, mkdir
 from pathlib import Path
 from colorama import init
@@ -10,7 +11,31 @@ from colorama import Fore, Back, Style
 init()
 
 Folder = 'Resources'
+Updater = 'Update Resources'
+Updater1 = 'Update Resources/Updater.py'
+Updater2 = 'Updater.py'
 
+
+if not path.exists(Updater):
+    mkdir(Updater)
+
+if not Path(Updater + "/Updater.py").is_file(): #Same here as above
+    tempd = requests.get("https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Updater.py")
+    with open(Updater + "/Updater.py", "wb") as f:
+        f.write(tempd.content)
+
+updateresult = filecmp.cmp(Updater1, Updater2, shallow=False)
+
+if updateresult == False:
+    sf = open(Updater1, "rb")
+    tf = open(Updater2,"wb")
+    tf.write(sf.read())
+    sf.close()
+    tf.close()
+    shutil.rmtree(Updater)
+
+if updateresult == True:
+    shutil.rmtree(Updater)
 
 if not path.exists(Folder):
     mkdir(Folder)  #If the Resource folder does not exist then it creates it
@@ -21,16 +46,16 @@ if not Path(Folder + "/values.json").is_file():   #If the values.json file does 
         f.write(tempd.content)  #Writes the values.json file
 
 with open(Folder + "/values.json", "r", encoding='utf-8') as values:
-    values = load(values) #Loads it
+    values = ujson.load(values) #Loads it
 
 response_Duino = requests.get('https://server.duinocoin.com/statistics') #Grabs Data
 Duino_Output = response_Duino.text
-parse_json = json.loads(Duino_Output) #Turns the API output into a python dictionary and then sets it as the variable parse_json
+parse_json = ujson.loads(Duino_Output) #Turns the API output into a python dictionary and then sets it as the variable parse_json
 DuinoPrice = parse_json['Duco price'] #Searches the dictionary for Duco price and sets DuinoPrice to the value
 
 def Convert(ID):
     Output = response.text
-    parse_json = json.loads(Output) #Turns the API output into a python dictionary and then sets it as the variable parse_json
+    parse_json = ujson.loads(Output) #Turns the API output into a python dictionary and then sets it as the variable parse_json
     Convert.Price = parse_json[ID]['usd'] #Goes through the ID variable then the usd to get the price
     return Convert.Price
 
