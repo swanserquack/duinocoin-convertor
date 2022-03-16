6b3bf3836da336b69e398806d36d19baa0045a79
swanserquack <75944176+swanserquack@users.noreply.github.com>
2022-03-16 17:37:49 +0000

    Formatted the code using black and fixed an id issue

--- Convertor.py
+++ Convertor.py
@@ -15,20 +15,23 @@ from os import path, mkdir
 from pathlib import Path
 from colorama import init
 from colorama import Fore, Back, Style
+
 init()
 
-Folder = 'Resources'
-Updater = 'Update Resources'
-Updater1 = 'Update Resources/Updater.py'
-Updater2 = 'Updater.py'
+Folder = "Resources"
+Updater = "Update Resources"
+Updater1 = "Update Resources/Updater.py"
+Updater2 = "Updater.py"
 values = "/values.json"
 
 
 if not path.exists(Updater):
     mkdir(Updater)
 
-if not Path(Updater + "/Updater.py").is_file(): #Same here as above
-    tempd = requests.get("https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Updater.py")
+if not Path(Updater + "/Updater.py").is_file():  # Same here as above
+    tempd = requests.get(
+        "https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Updater.py"
+    )
     with open(Updater + "/Updater.py", "wb") as f:
         f.write(tempd.content)
 
@@ -36,7 +39,7 @@ updateresult = filecmp.cmp(Updater1, Updater2, shallow=False)
 
 if updateresult == False:
     sf = open(Updater1, "rb")
-    tf = open(Updater2,"wb")
+    tf = open(Updater2, "wb")
     tf.write(sf.read())
     sf.close()
     tf.close()
@@ -46,78 +49,107 @@ if updateresult == True:
     shutil.rmtree(Updater)
 
 if not path.exists(Folder):
-    mkdir(Folder)  #If the Resource folder does not exist then it creates it
+    mkdir(Folder)  # If the Resource folder does not exist then it creates it
 
-if not Path(Folder + values).is_file():   #If the values.json file does not exist then it creates it
-    tempd = requests.get("https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Resources/values.json")
+if not Path(
+    Folder + values
+).is_file():  # If the values.json file does not exist then it creates it
+    tempd = requests.get(
+        "https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Resources/values.json"
+    )
     with open(Folder + values, "wb") as f:
-        f.write(tempd.content)  #Writes the values.json file
+        f.write(tempd.content)  # Writes the values.json file
 
-with open(Folder + values, "r", encoding='utf-8') as values:
-    values = ujson.load(values) #Loads it
+with open(Folder + values, "r", encoding="utf-8") as values:
+    values = ujson.load(values)  # Loads it
 
 
-response_Duino = requests.get('https://server.duinocoin.com/statistics') #Grabs Data
+response_Duino = requests.get("https://server.duinocoin.com/statistics")  # Grabs Data
 Duino_Output = response_Duino.text
-parse_json = ujson.loads(Duino_Output) #Turns the API output into a python dictionary and then sets it as the variable parse_json
-DuinoPrice = parse_json['Duco price'] #Searches the dictionary for Duco price and sets DuinoPrice to the value
+parse_json = ujson.loads(
+    Duino_Output
+)  # Turns the API output into a python dictionary and then sets it as the variable parse_json
+DuinoPrice = parse_json[
+    "Duco price"
+]  # Searches the dictionary for Duco price and sets DuinoPrice to the value
 
 
-def convert(id):
+def convert(ID):
     output = response.text
-    parse_json = ujson.loads(output) #Turns the API output into a python dictionary and then sets it as the variable parse_json
-    convert.Price = parse_json[id]['usd'] #Goes through the ID variable then the usd to get the price
+    parse_json = ujson.loads(
+        output
+    )  # Turns the API output into a python dictionary and then sets it as the variable parse_json
+    convert.Price = parse_json[ID][
+        "usd"
+    ]  # Goes through the ID variable then the usd to get the price
     return convert.Price
 
-def calculationto(id):
-    duinoamount=float(input("How many Duino-Coins do you have?"))
-    duinototal=DuinoPrice*duinoamount
-    finaloutput = float(duinototal/float(convert.Price)) #Floats the CurrencyPrice variable (Due to it being a string on the output) and floats the overall thing.
-    print('You would have', finaloutput ,'amount of', id, '\n')
-        
+
+def calculationto(ID):
+    duinoamount = float(input("How many Duino-Coins do you have?"))
+    duinototal = DuinoPrice * duinoamount
+    finaloutput = float(
+        duinototal / float(convert.Price)
+    )  # Floats the CurrencyPrice variable (Due to it being a string on the output) and floats the overall thing.
+    print("You would have", finaloutput, "amount of", ID, "\n")
+
 
 def calculationfrom():
-    coinamount=float(input("How many coins do you have?"))
-    cointotal=float(convert.Price)*coinamount
-    finaloutput = float(cointotal/DuinoPrice) #Floats the CoinTotal, Divides the CoinTotal by the DuinoPrice and floats it.
-    print('You would have', finaloutput, 'duino-coin\n')
+    coinamount = float(input("How many coins do you have?"))
+    cointotal = float(convert.Price) * coinamount
+    finaloutput = float(
+        cointotal / DuinoPrice
+    )  # Floats the CoinTotal, Divides the CoinTotal by the DuinoPrice and floats it.
+    print("You would have", finaloutput, "duino-coin\n")
+
 
 def nosupport():
     print("Currency is not currently supported, open an issue to get it added.")
 
 
 while True:
-    print(Fore.YELLOW + 'Duino Coin Convertor')
-    print('')
-    Option = input(Style.RESET_ALL + Fore.CYAN + '1 - Convert from Duino-Coin\n' + Style.RESET_ALL + Fore.GREEN + '2 - Convert to Duino-Coin\n' + Style.RESET_ALL + Fore.MAGENTA + '3 - Quit\n' + Style.RESET_ALL)
+    print(Fore.YELLOW + "Duino Coin Convertor")
+    print("")
+    Option = input(
+        Style.RESET_ALL
+        + Fore.CYAN
+        + "1 - Convert from Duino-Coin\n"
+        + Style.RESET_ALL
+        + Fore.GREEN
+        + "2 - Convert to Duino-Coin\n"
+        + Style.RESET_ALL
+        + Fore.MAGENTA
+        + "3 - Quit\n"
+        + Style.RESET_ALL
+    )
 
-    if Option == '1':
-        Currency=input("What currency do you want to convert to?")
+    if Option == "1":
+        Currency = input("What currency do you want to convert to?")
 
         for option in values["currency_options"]:
-            if Currency.lower() in option['names']:
-                id = option['id']
-                URL = option['url']
+            if Currency.lower() in option["names"]:
+                ID = option["id"]
+                URL = option["url"]
                 response = requests.get(URL)
-                convert(id)
-                calculationto(id)
-                break  
+                convert(ID)
+                calculationto(ID)
+                break
         else:
             nosupport()
 
-    elif Option == '2':
-        Currency=input("What currency do you want to convert from?")
-        
+    elif Option == "2":
+        Currency = input("What currency do you want to convert from?")
+
         for option in values["currency_options"]:
-            if Currency.lower() in option['names']:
-                id = option['id']
-                URL = option['url']
+            if Currency.lower() in option["names"]:
+                ID = option["id"]
+                URL = option["url"]
                 response = requests.get(URL)
-                convert(id)
+                convert(ID)
                 calculationfrom()
                 break
         else:
             nosupport()
-            
-    elif Option == '3':
+
+    elif Option == "3":
         quit()
--- Updater.py
+++ Updater.py
@@ -13,16 +13,16 @@ import filecmp
 from os import path, mkdir
 from pathlib import Path
 
-#Sets all the values we need
-Updater = 'Update Resources'
-Folder = 'Resources'
-json1 = 'Resources/values.json'
-json2 = 'Update Resources/values.json'
-python1 = 'Convertor.py'
-python2 = 'Update Resources/Convertor.py'
-req1 = 'requirements.txt'
-req2 = 'Update Resources/requirements.txt'
-Complete = 'Update Complete'
+# Sets all the values we need
+Updater = "Update Resources"
+Folder = "Resources"
+json1 = "Resources/values.json"
+json2 = "Update Resources/values.json"
+python1 = "Convertor.py"
+python2 = "Update Resources/Convertor.py"
+req1 = "requirements.txt"
+req2 = "Update Resources/requirements.txt"
+Complete = "Update Complete"
 values = "/values.json"
 
 if not path.exists(Updater):
@@ -31,45 +31,57 @@ if not path.exists(Updater):
 if not path.exists(Folder):
     mkdir(Folder)
 
-if not Path(Folder + values).is_file():   #If the values.json file does not exist then it creates it
-    tempd = requests.get("https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Resources/values.json")
+if not Path(
+    Folder + values
+).is_file():  # If the values.json file does not exist then it creates it
+    tempd = requests.get(
+        "https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Resources/values.json"
+    )
     with open(Folder + values, "wb") as f:
-        f.write(tempd.content)  #Writes the values.json file
+        f.write(tempd.content)  # Writes the values.json file
 
-#Grabs the update files fromt the repository
+# Grabs the update files fromt the repository
 
 if not Path(Updater + values).is_file():
-    tempd = requests.get("https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Resources/values.json")
+    tempd = requests.get(
+        "https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Resources/values.json"
+    )
     with open(Updater + values, "wb") as f:
         f.write(tempd.content)
 
 if not Path(Updater + "/Convertor.py").is_file():
-    tempd = requests.get("https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Convertor.py")
+    tempd = requests.get(
+        "https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/Convertor.py"
+    )
     with open(Updater + "/Convertor.py", "wb") as f:
         f.write(tempd.content)
 
 if not Path(Updater + "/requirements.txt").is_file():
-    tempd = requests.get("https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/requirements.txt")
+    tempd = requests.get(
+        "https://raw.githubusercontent.com/swanserquack/duinocoin-convertor/main/requirements.txt"
+    )
     with open(Updater + "/requirements.txt", "wb") as f:
         f.write(tempd.content)
 
-#Checks for differences
+# Checks for differences
 pyresult = filecmp.cmp(python1, python2, shallow=False)
 jsonresult = filecmp.cmp(json1, json2, shallow=False)
 reqresult = filecmp.cmp(req1, req2, shallow=False)
 
 if jsonresult == False:
-    print('Json file is not up to date, updating now...')
+    print("Json file is not up to date, updating now...")
     sf = open(json2, "rb")
-    tf = open(json1,"wb")
+    tf = open(json1, "wb")
     tf.write(sf.read())
     sf.close()
     tf.close()
     print(Complete)
-    jsonresult = filecmp.cmp(json1, json2, shallow=False) #Recheck to make sure the update completed and jsonresult is set to true
+    jsonresult = filecmp.cmp(
+        json1, json2, shallow=False
+    )  # Recheck to make sure the update completed and jsonresult is set to true
 
 if pyresult == False:
-    print('Python file is not up to date, updating now...')
+    print("Python file is not up to date, updating now...")
     pf = open(python2, "rb")
     kf = open(python1, "wb")
     kf.write(pf.read())
@@ -79,7 +91,7 @@ if pyresult == False:
     pyresult = filecmp.cmp(python1, python2, shallow=False)
 
 if reqresult == False:
-    print('Requirements file is not up to date, updating now...')
+    print("Requirements file is not up to date, updating now...")
     uf = open(req2, "rb")
     of = open(req1, "wb")
     of.write(uf.read())
@@ -89,8 +101,10 @@ if reqresult == False:
     reqresult = filecmp.cmp(req1, req2, shallow=False)
 
 
-if pyresult == True and jsonresult == True and reqresult == True:  #After the recheck they all are True and the update is complete
-    print('Files are up to date, closing in 10 seconds')
+if (
+    pyresult == True and jsonresult == True and reqresult == True
+):  # After the recheck they all are True and the update is complete
+    print("Files are up to date, closing in 10 seconds")
     shutil.rmtree(Updater)
     time.sleep(10)
     quit()
